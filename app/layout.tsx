06f93105493ada6633797b96686e114615641419
João Vitor de Aquino <jvaquinocontact@gmail.com>
2025-05-07 13:26:21 -0300

    (front/web) feat: add base design system & react-hot-toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Public_Sans } from "next/font/google";
 import "./globals.css";
+import { ToastProvider } from "@/components/common/ToastProvider";
 
 const publicSans = Public_Sans({
   variable: "--font-public",
@@ -23,6 +24,8 @@ export default function RootLayout({
         className={`${publicSans.variable} antialiased`}
       >
         {children}
+
+        <ToastProvider />
       </body>
     </html>
   );
